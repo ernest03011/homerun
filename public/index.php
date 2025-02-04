8054ef20c99203b61eda20354299852080563635
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-02-04 10:51:44 -0400

    Remove hello world message and add a simple get request to get the roster for a team in 2025

--- public/index.php
+++ public/index.php
@@ -4,4 +4,12 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-echo 'Hello World!';
+use  App\ApiCallHandler;
+
+$data =  (new ApiCallHandler)->execute(
+    'https://statsapi.mlb.com/api/v1/teams/135/roster?season=2025',
+);
+
+echo '<pre>';
+var_dump($data);
+echo '</pre>';
